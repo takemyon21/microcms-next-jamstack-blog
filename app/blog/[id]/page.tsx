42dbc0f8002cd516a22ecbf061ca202d3e059d0d
EVO-X1\take <diver_takemyon@hotmail.com>
2025-08-28 22:45:15 +0900

    テストアップロード

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,48 @@
+// app/blog/[id]/page.tsx
+import { client } from "../../../libs/microcms";
+import dayjs from "dayjs";
+
+// ブログ記事の型定義
+type Props = {
+  id: string;
+  title: string;
+  content: string;
+  publishedAt: string;
+  category: { name: string };
+};
+
+// microCMSから特定の記事を取得
+async function getBlogPost(id: string): Promise<Props> {
+  const data = await client.get({
+    endpoint: `blog/${id}`,
+  });
+  return data;
+}
+
+// 記事詳細ページの生成
+export default async function BlogPostPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params; // IDを取得
+  const post = await getBlogPost(id);
+
+  // dayjsを使ってpublishedAtをYY.MM.DD形式に変換
+  const formattedDate = dayjs(post.publishedAt).format("YY.MM.DD");
+  const markup = { __html: post.content };
+
+  return (
+    <main>
+      <h1>{post.title}</h1> {/* タイトルを表示 */}
+      <div>{formattedDate}</div> {/* 日付を表示 */}
+      <div>カテゴリー：{post.category && post.category.name}</div> {/* カテゴリーを表示 */}
+      <div dangerouslySetInnerHTML={markup} /> {/* 記事本文を表示 */}
+    </main>
+  );
+}
+
+// 静的パスを生成
+export async function generateStaticParams() {
+  const contentIds = await client.getAllContentIds({ endpoint: "blog" });
+
+  return contentIds.map((contentId) => ({
+    id: contentId, // 各記事のIDをパラメータとして返す
+  }));
+}
